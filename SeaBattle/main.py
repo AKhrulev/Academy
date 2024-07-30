121f6b0ee84452a87ff1ee80c1e95dca5c286179
Alexey.Khrulev <alexey52@gmail.com>
2024-07-30 21:26:07 +0300

    Implement add_ship() and auxiliary functions

--- SeaBattle/main.py
+++ SeaBattle/main.py
@@ -1,21 +1,14 @@
 from impl import *
 
-N = 3  # размер стороны игрового поля
+N = 7  # размер стороны игрового поля
 fields = init_fields(2, N)
-draw_fields(fields)
+# draw_fields(fields)
 
 # Добавляем корабль
-is_success = add_ship(fields[0], 2, ('А',2), False)
+is_success = add_ship(fields[0], 2, ('А',2), True)
 if is_success:
     print('Корабль добавлен')
 
 draw_fields(fields)
 
-#     1 2 3    1 2 3
-#   А . . .  А . . .
-#   Б . . .  Б . . .
-#   В . . .  В . . .
-
-
-
-
+exit()
